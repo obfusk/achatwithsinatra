6eaa953bd6443c457728378d99e921e3deccd251
Felix C. Stegerman <flx@obfusk.net>
2013-12-09 21:59:15 +0100

    stripped!

--- achatwithsinatra.rb
+++ achatwithsinatra.rb
@@ -20,8 +20,7 @@ class AChatWithSinatra < Sinatra::Base
   class UnknownUser < RuntimeError; end
   class EmptyChannel < RuntimeError; end
 
-  BLANK_STATE     = -> { { channels: {}, users: {}, n: 0 } }
-  DEBUG           = ENV['ACHATWITHSINATRA_CUKE'] == 'yes'
+  BLANK_STATE     = -> { ... }
   DEFAULT_CHANNEL = 'devnull'
   KEEP_ALIVE      = 15
   LOGIN_TIMEOUT   = 15
@@ -43,166 +42,13 @@ class AChatWithSinatra < Sinatra::Base
     ''
   end
 
-  def send(out, event, data = {})
-    d = data.merge time: Time.new.strftime('%F %T')
-    out << "event: #{event}\ndata: #{d.to_json}\n\n"
-    puts "event: #{event}\ndata: #{d.to_json}\n\n" if DEBUG
-  end
-
-  def send_from(user, event, msg)
-    channel(user[:channel]).each { |out| send out, event, msg }
-  end
-
-  def channel(c)
-    settings.state[:channels][c] ||= []
-  end
-
-  def set_channel(user, c)
-    raise EmptyChannel if c.empty?
-    unless (old = user[:channel]) == c
-      user[:channel] = c
-      channel(old).delete user[:_conn]
-      channel(c) << user[:_conn]
-    end
-  end
-
-  def cleanup(user)
-    user[:_timer].cancel
-    channel(user[:channel]).delete user[:_conn]
-    rm_user user
-  end
-
-  def rm_user(user)
-    settings.state[:users].delete user[:id]
-  end
-
-  def add_user
-    user = new_user; settings.state[:users][user[:id]] = user
-    EM::Timer.new(LOGIN_TIMEOUT) { rm_user user unless user[:_conn] }
-    user
-  end
-
-  def connect_user(user, out)
-    c = DEFAULT_CHANNEL
-    t = EM::PeriodicTimer.new(KEEP_ALIVE) { send out, :ping }
-    user.merge! _conn: out, _timer: t, channel: c
-    channel(c) << out; out.callback { cleanup user }
-    [user, c]
-  end
-
-  def new_user
-    n = settings.state[:n] += 1
-    { id: new_id(n), nick: "guest#{n}" }
-  end
-
-  def get_user(id)
-    settings.state[:users][id] || raise(UnknownUser, id)
-  end
-
-  def all_nicks
-    settings.state[:users].values.map { |x| x[:nick] }
-  end
-
-  def set_nick(id, nick)
-    if nick.empty?
-      { error: 'empty nick' }
-    elsif nick =~ /^guest\d+$/
-      { error: 'is a guest nick' }
-    elsif all_nicks.include? nick
-      { error: 'nick is taken' }
-    else
-      settings.state[:users][id][:nick] = nick
-      { nick: nick }
-    end
-  end
-
-  def sanitize(h)
-    h.reject { |x| x.to_s.start_with? '_' }
-  end
-
-  def join_say_me(event, &b)
-    data = json_body; user = get_user data['id']; d = b[user, data]
-    send_from user, event, { nick: user[:nick] }.merge(d)
-    empty_response
-  end
-
-  if DEBUG
-    def new_id(n)
-      "SecureRandom-#{n}"
-    end
-
-    before do
-      s = settings.state
-      puts "n: #{s[:n]}; channels: #{s[:channels].keys*', '}"
-      puts 'users:'
-      s[:users].each do |k,v|
-        v2 = v.merge _conn: v[:_conn].class, _timer: v[:_timer].class
-        puts "  #{k} => #{v2}"
-      end
-    end
-
-    post '/reset' do
-      settings.state = BLANK_STATE[]
-      empty_response
-    end
-  else
-    def new_id(n)
-      SecureRandom.hex 32
-    end
-  end
+  ...
 
   get '/' do
     haml :ui
   end
 
-  get '/channels' do
-    content_type :json
-    settings.state[:channels].keys.sort.to_json
-  end
-
-  get '/events' do
-    content_type :json
-    id = add_user[:id]
-    { location: "/events/#{id}", id: id }.to_json
-  end
-
-  get '/events/:id' do |id|
-    user = get_user id
-    content_type 'text/event-stream'
-    stream(:keep_open) do |out|
-      user, ch = connect_user user, out
-      send out, :welcome, sanitize(user)
-      send_from user, :join, nick: user[:nick], channel: ch
-    end
-  end
-
-  post '/join' do
-    join_say_me(:join) do |user, data|
-      set_channel user, data['channel'].strip
-      { channel: data['channel'] }
-    end
-  end
-
-  post '/say' do
-    join_say_me(:say) do |user, data|
-      { message: data['message'] }
-    end
-  end
-
-  post '/me' do
-    join_say_me(:me) do |user, data|
-      { message: data['message'] }
-    end
-  end
-
-  post '/nick' do
-    content_type :json
-    data = json_body  ; user  = get_user data['id']
-    from = user[:nick]; res   = set_nick user[:id], data['nick'].strip
-    send_from user, :nick, from: from, to: res[:nick] \
-      unless res[:error]
-    res.to_json
-  end
+  ...
 
   get '/__coffee__/:name.js' do |name|
     content_type :js
